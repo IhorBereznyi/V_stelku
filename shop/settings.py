b48e62c1f3037822a796481471299c32b14beafe
Ihor <igorbereznij90@gmail.com>
2021-04-23 11:02:05 +0300

    Commit 1

--- shop/settings.py
+++ shop/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vstelku_db',
+#         'USER': 'Ihor',
+#         'PASSWORD': 'Ihor2112',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432  
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
